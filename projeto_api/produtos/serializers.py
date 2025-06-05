264995a92a4e4ff164a3b0e3ebbac36ac7793605
SouOMestreDosMagos <enzogonzalesxavier.dev2024@gmail.com>
2025-06-05 09:56:46 -0300

    ddd

--- projeto_api/produtos/serializers.py
+++ projeto_api/produtos/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import Comida
+from .models import Pedido
+
 class ComidaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comida
         fields = '__all__'  
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
--- projeto_api/produtos/urls.py
+++ projeto_api/produtos/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from .views import criar_pedido
+
 urlpatterns = [
     path('comidas/', views.ComidaList.as_view(), name='comida-list'),
     path('comidas/<int:pk>/', views.ComidaDetail.as_view(), name='comida-detail'),
+]
+
+urlpatterns = [
+    path('api/pedidos/', criar_pedido, name='criar_pedido'),
 ]
